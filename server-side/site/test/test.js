e60f07f23a7556405f459920d8e739f168e5f323
Charan Ram <35636004+cvellai@users.noreply.github.com>
2019-02-23 15:21:40 -0500

    Update test.js

--- server-side/site/test/test.js
+++ server-side/site/test/test.js
@@ -4,7 +4,7 @@ const got      = require('got');
 
 describe('checkbox.io', () => {
   describe('/api/design/survey', () => {
-    it('should return a rendered output', async () => {
+    it('should properly create survey question', async () => {
       const response = await got.post('http://192.168.10.3:8008/api/design/survey', {
         body: {
 	"markdown": "{NumberQuestions:true}\n-----------\n### Multiple Choice Question (Check all that apply)\nA *description* for question.\nQuestions are created with headers (level 3) `### Multiple Choice Question (Check all that apply)`.\n* Choice A\n* Choice B\n* Choice C"
