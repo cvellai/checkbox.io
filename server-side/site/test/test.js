ae7da032a1df83aa96ccd0cc9cc1ab852e46dbf5
Charan Ram <35636004+cvellai@users.noreply.github.com>
2019-02-23 15:20:59 -0500

    Update test.js

--- server-side/site/test/test.js
+++ server-side/site/test/test.js
@@ -4,11 +4,16 @@ const got      = require('got');
 
 describe('checkbox.io', () => {
   describe('/api/design/survey', () => {
-    it('should return expected output', async () => {
-      const response = await got.post('http://localhost:3001/api/design/survey', {
-
+    it('should return a rendered output', async () => {
+      const response = await got.post('http://192.168.10.3:8008/api/design/survey', {
+        body: {
+	"markdown": "{NumberQuestions:true}\n-----------\n### Multiple Choice Question (Check all that apply)\nA *description* for question.\nQuestions are created with headers (level 3) `### Multiple Choice Question (Check all that apply)`.\n* Choice A\n* Choice B\n* Choice C"
+},
+        json: true,
+        timeout: 500
       });
 
+      expect(response.body).to.include('Multiple Choice Question');
     });
   });
 });
