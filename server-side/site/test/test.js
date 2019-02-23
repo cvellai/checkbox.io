a24309ba17344cfa609ce5c5b3b1aa1ca825e41d
Charan Ram <35636004+cvellai@users.noreply.github.com>
2019-02-23 13:50:01 -0500

    Update test.js

--- server-side/site/test/test.js
+++ server-side/site/test/test.js
@@ -6,15 +6,9 @@ describe('checkbox.io', () => {
   describe('/api/design/survey', () => {
     it('should return expected output', async () => {
       const response = await got.post('http://localhost:3001/api/design/survey', {
-        body: {
-          markdown: '{NumberQuestions:true}\n----------------------\n# Test'
-        },
-        json: true,
-        timeout: 500
+
       });
 
-      expect(response.body).to.have.property('preview');
-      expect(response.body.preview).to.equal('<p><div data-page="0"></p>\n<h1 id="test">Test</h1>\n<p></div></p>\n');
     });
   });
 });
